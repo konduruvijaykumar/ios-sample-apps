b179fddf92febce40f8fbc274c7e521a69bef9d4
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-02-08 01:45:04 +0530

    Vijay - 2017/02/08 - background image and appicon set

---
+++ BackgroundImageApp/BackgroundImageApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BackgroundImageApp
+//
+//  Created by Vijay Konduru on 31/01/17.
+//  Copyright © 2017 PJay. All rights reserved.
+//
+
+// To create your icon set for ios app, you need to have min 1024 * 1024 size icon and which can be uploaded in website or app to get right icons
+// https://makeappicon.com/
+// You can try "Icon Set Creator" app for mac
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
