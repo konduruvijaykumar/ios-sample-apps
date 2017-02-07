a2ef34ae2124156fe63484d3e1f5c6dce499f607
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-02-08 04:25:41 +0530

    Vijay - 2017/02/08 - adding tabbed app demo

---
+++ TabbedAppDemo/TabbedAppDemo/FavoritesViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  TabbedAppDemo
+//
+//  Created by Vijay Konduru on 05/02/17.
+//  Copyright © 2017 PJay. All rights reserved.
+//
+
+// https://www.youtube.com/watch?v=B9yV4YeEmNA
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
