09be037c539091383bc12a5a861d5935e820c149
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-02-21 04:29:04 +0530

    Vijay - 2017/02/12 - All splash or launch screen demo

---
+++ SplashScreenDemo/SplashScreenDemo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SplashScreenDemo
+//
+//  Created by Vijay Konduru on 16/02/17.
+//  Copyright © 2017 PJay. All rights reserved.
+//
+
+//https://developer.apple.com/ios/human-interface-guidelines/overview/design-principles/
+//https://www.appcoda.com/how-to-add-splash-screen-in-your-ios-app/
+//http://stackoverflow.com/questions/5618163/displaying-splash-screen-for-longer-than-default-seconds
+//http://stackoverflow.com/questions/35028449/ios-how-to-delay-the-launch-screen
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
